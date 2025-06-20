905141627ebab2930d16033eed883668e96a5c68
roeiV789 <roei_vager@DESKTOP-IO9EV9D.localdomain>
2025-06-20 23:44:09 +0300

    fixed logic with shared pointers in all functions

--- UnionFind.cpp
+++ UnionFind.cpp
@@ -1,56 +1,88 @@
 #include "UnionFind.h"
 
 
-template<class Group, class Elem>
-typename UnionFind<Group,Elem>::Node* UnionFind<Group,Elem>::Node::findRoot(Node* current) {
-    if(!current->parent) {
-        return current;
+template<class Elem>
+ElementNode<Elem> *ElementNode<Elem>::findRoot() {
+    if (!parent) {
+        return this;
     }
-    current->parent = findRoot(current->parent);
-    return current->parent; //path compression optimization
+    parent = parent->findRoot(); //path compression optimization
+    return parent;
 }
-template<class Group, class Elem>
-UnionFind<Group,Elem>::UnionFind() = default;
 
 template<class Group, class Elem>
-UnionFind<Group,Elem>::~UnionFind() = default;
+UnionFind<Group, Elem>::UnionFind() = default;
 
 template<class Group, class Elem>
-int UnionFind<Group,Elem>::find(int elementId){
-    auto nodePtr = elementsTable.hash(elementId);
-    if(!nodePtr) {
+UnionFind<Group, Elem>::~UnionFind() = default;
+
+template<class Group, class Elem>
+int UnionFind<Group, Elem>::find(int elementId) {
+    //check if element is in the structure
+    auto elementPtr = elementsTable.hash(elementId);
+    if (!elementPtr) {
         return -1;
     }
-    auto rootPtr= std::make_shared<Node>(Node::findRoot(nodePtr.get()));
-    return rootPtr->groupId;
+    //find the root of the group
+    return elementPtr->findRoot()->groupId;
 }
+
+//creates an empty group
 template<class Group, class Elem>
-bool UnionFind<Group,Elem>::makeSet(int elementId, int groupId, const Elem& newElement) {
-    auto groupPtr = groupsTable.hash(groupId);
-    if(!groupPtr||elementsTable.hash(elementId)) {
+bool UnionFind<Group, Elem>::addGroup(int groupId, const Group &group) {
+    if (groupsTable.hash(groupId)) {
         return false;
     }
-    auto nodePtr = make_shared<Node>(newElement,groupId);
-    nodePtr->parent = groupPtr->srcPtr;
-    elementsTable.insert(elementId, nodePtr);
+    std::shared_ptr<GroupNode<Group, Elem> > newGroup = make_shared<GroupNode<Group, Elem> >(group, groupId);
+    groupsTable.insert(newGroup);
     return true;
-
 }
-//groupId1 should house the bigger group
+
 template<class Group, class Elem>
-bool UnionFind<Group,Elem>::unionGroups(int groupId1, int groupId2, int groupId3) {
+bool UnionFind<Group, Elem>::addElement(int elementId, const Elem &element, int groupId) {
+    auto elementPtr = elementsTable.hash(elementId);
+    if (elementPtr) {
+        return false;
+    }
+    auto groupPtr = groupsTable.hash(groupId);
+    if (!groupPtr) {
+        return false;
+    }
+    std::shared_ptr<ElementNode<Elem> > newElement = make_shared<ElementNode<Elem> >(element, groupId);
+    if (groupPtr->size == 0) {
+        groupPtr->root = elementPtr.get();
+        groupPtr->size++;
+    } else {
+        newElement->parent = groupPtr->root;
+    }
+    return true;
+}
+
+template<class Group, class Elem>
+bool UnionFind<Group, Elem>::unionGroups(int groupId1, int groupId2, int groupId3) {
     auto rootPtr1 = groupsTable.hash(groupId1);
     auto rootPtr2 = groupsTable.hash(groupId2);
-    if(!groupsTable.hash(groupId1)||!groupsTable.hash(groupId2)||groupsTable.hash(groupId3)) {
+    if (!groupsTable.hash(groupId1) || !groupsTable.hash(groupId2) || groupsTable.hash(groupId3)) {
         return false;
     }
-    if(rootPtr1==rootPtr2) {
+    if (rootPtr1->groupId == rootPtr2->groupId) {
         return false;
     }
-    groupsTable.insert(groupId3,rootPtr1);
-    rootPtr2->parent = rootPtr1;
+    std::shared_ptr<GroupNode<Group, Elem> > mergedGroup = make_shared<GroupNode<Group, Elem> >(Group(), groupId3);
+    mergedGroup->size = rootPtr1->size + rootPtr2->size;
+    groupsTable.insert(groupId3, mergedGroup);
+    if (rootPtr1->size >= rootPtr2->size) {
+        mergedGroup->root = rootPtr1->root;
+        rootPtr2->parent = mergedGroup->root;
+    } else {
+        mergedGroup->root = rootPtr2->root;
+        rootPtr1->parent = mergedGroup->root;
+    }
+    //the groups are now empty
+    rootPtr1.size=0;
+    rootPtr2.size=0;
     //should change this based on how hash table is implemented
-    groupsTable.hash(groupId1)=nullptr;
-    groupsTable.hash(groupId2)=nullptr;
+    groupsTable.hash(groupId1) = nullptr;
+    groupsTable.hash(groupId2) = nullptr;
     return true;
 }
--- UnionFind.h
+++ UnionFind.h
@@ -1,26 +1,40 @@
 #pragma once
 #include "HashTab.h"
 #include <memory>
-    template<class Group, class Elem>
-    class UnionFind {
-        struct Node{
-            Elem element; //so this would be song
-            Node* parent;
-            Node* srcPtr;
-            int groupId;
-            explicit Node(const Elem& element, int groupId) : element(element), parent(nullptr),
-                          srcPtr(nullptr) ,groupId(groupId) {}
-            Node* findRoot(Node* current);
-        };
-        HashTab<int, std::shared_ptr<Node>> elementsTable;
-        HashTab<int, std::unique_ptr<Group>> groupsTable;
+template<class Elem>
+
+struct ElementNode{
+    Elem& element; //so this would be song
+    ElementNode* parent;
+    int groupId;
+    ElementNode(const Elem& element, int groupId) : element(element), parent(nullptr),
+                  groupId(groupId) {}
+    ElementNode* findRoot();
+};
+template<class Group, class Elem>
+struct GroupNode{
+    Group& group; //so this would be song
+    ElementNode<Elem>* root;
+    int groupId;
+    int size;
+    GroupNode(const Group& group, int groupId) : group(group), root(nullptr),
+                  groupId(groupId), size(0) {}
+};
+
+
+template<class Group, class Elem>
+class UnionFind {
+
+    HashTab<int, std::shared_ptr<ElementNode<Elem>>> elementsTable;
+    HashTab<int, std::unique_ptr<GroupNode<Group,Elem>>> groupsTable;
 
     public:
         UnionFind();
         ~UnionFind();
         int find(int elementId);
-        bool makeSet(int elementId, int groupId, const Elem& newElement);
+        bool addElement(int elementId, const Elem &element, int groupId);
+        bool addGroup(int groupId, const Group &group);
         bool unionGroups(int groupId1, int groupId2, int groupId3);
-    };
 
+};
 
